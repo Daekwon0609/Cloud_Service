50c3d29fee985638cc4e39aa11cf952fd8786659
대권 <rlaeornjs55@gmail.com>
2021-11-16 00:53:10 +0900

    Time Change

--- utils/check.py
+++ utils/check.py
@@ -13,7 +13,7 @@ async def check_config(bot: discord.Client):
     await cur.execute("SELECT Category, Type FROM cloud_setup")
     list_setup_value = await cur.fetchall()
 
-    if list_setup_value == 0:
+    if len(list_setup_value) == 0:
         return False
     else:  
         for value in list_setup_value:
@@ -26,31 +26,26 @@ async def check_config(bot: discord.Client):
                 if category == None:
                     cn_value_2 = change_name(value[1])
                     value_none_list.append(cn_value_2)
+        await cur.close()
+        return value_none_list
 
 async def check_channel(bot: discord.Client):
-    print("채널 확인 스크립트 시작.")
+    print("충돌 확인 스크립트 시작.")
+    cur = await connect_db()
     while True:
-        now = datetime.now()
-        time = f"{now.minute}:{now.second}"
+        await cur.execute("SELECT Channel FROM cloud_service")
+        list_channel_value = await cur.fetchall()
 
-        if time == "0:0":
-            cur = await connect_db()
+        if len(list_channel_value) == 0:
+            pass
 
-            await cur.execute("SELECT Channel FROM cloud_service")
-            list_channel_value = await cur.fetchall()
+        for channel_id in list_channel_value:
+            true = bot.get_channel(id=channel_id[0])
 
-            for channel_id in list_channel_value:
-                true = bot.get_channel(id=channel_id[0])
-
-                if true == None:
-                    await cur.execute("DELETE FROM cloud_service WHERE Channel = ?", (channel_id[0],))
-                    await log_pr(f"오류 제거: ID - {channel_id[0]}")
-                else:
-                    return
+            if true == None:
+                await cur.execute("DELETE FROM cloud_service WHERE Channel = ?", (channel_id[0],))
+                await log_pr(f"충돌 제거: ID - {channel_id[0]}")
+            else:
+                pass
         await asyncio.sleep(1)
-
-
-    
-    await cur.close()
-    return value_none_list
         
--- utils/restart.py
+++ utils/restart.py
@@ -9,7 +9,7 @@ async def restart_system():
         now = datetime.now()
         time = f"{now.hour}:{now.minute}:{now.second}"
 
-        if time == "0:0:0":
+        if time == "0:30:0":
             os.system("start python -B -m cloudsb")
             os.system("cls")
             print("[과부하 방지] 시스템을 새로 다시 시작하였습니다.\n\n[과부하 방지] 창을 닫아주세요.")
