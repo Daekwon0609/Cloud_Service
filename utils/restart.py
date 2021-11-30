fb02d2d0df948d07bd00ceddabdd8a6080ea3810
대권 <rlaeornjs55@gmail.com>
2021-11-30 20:35:15 +0900

    Update restart.py

--- utils/restart.py
+++ utils/restart.py
@@ -7,9 +7,9 @@ async def restart_system():
     print("과부하 방지 스크립트 시작.")
     while True:
         now = datetime.now()
-        time = f"{now.minute}:{now.second}"
+        time = f"{now.hour}:{now.minute}:{now.second}"
 
-        if time == "30:0":
+        if time == "3:30:0":
             os.system("start python -B -m cloudsb")
             os.system("cls")
             print("[과부하 방지] 시스템을 새로 다시 시작하였습니다.\n\n[과부하 방지] 창을 닫아주세요.")
